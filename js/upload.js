a2f44266a8792794897521e5c8120ea6e5c2f26c
turhanberk <berkturhan@sabanciuniv.edu>
2022-09-03 01:36:33 +0300

    states change update

--- js/upload.js
+++ js/upload.js
@@ -1,111 +1,3 @@
-function datatype(){
-    sessionStorage.setItem("vis_perm",false)
-    var i1 = sessionStorage.getItem("source_color")
-    var i2 = sessionStorage.getItem("source_size")
-    var i3 = sessionStorage.getItem("target_phos")
-    var i4 = sessionStorage.getItem("phos_color")
-    var i5 = sessionStorage.getItem("phos_size")
-    var i6 = sessionStorage.getItem("target_color")
-    var i7 = sessionStorage.getItem("target_size")
-    var i8 = sessionStorage.getItem("edge_color")
-    var i9 = sessionStorage.getItem("edge_weight")
-
-    if(i3=="true"){
-        document.getElementById("pt_table").style.display = "table";
-        if(i1!="true"){
-            Array.from(document.getElementsByClassName("p2")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i2 != "true"){
-            Array.from(document.getElementsByClassName("p3")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i4!="true"){
-            Array.from(document.getElementsByClassName("p6")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i5!="true"){
-            Array.from(document.getElementsByClassName("p7")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i8!="true"){
-            Array.from(document.getElementsByClassName("p8")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i9!="true"){
-            Array.from(document.getElementsByClassName("p9")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-
-
-
-    }
-    else{
-        document.getElementById("npt_table").style.display = "table";
-        if(i1!="true"){
-            Array.from(document.getElementsByClassName("np2")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i2!="true"){
-            Array.from(document.getElementsByClassName("np3")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i6!="true"){
-            Array.from(document.getElementsByClassName("np5")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i7!="true"){
-            Array.from(document.getElementsByClassName("np6")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i8!="true"){
-            Array.from(document.getElementsByClassName("np7")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-        if(i9!="true"){
-            Array.from(document.getElementsByClassName("np8")).forEach(
-                function(element, index, array) {
-                    element.style.display = "none"
-                }
-            );
-        }
-
-    }
-
-}
 function direct_initiate(){
     sessionStorage.setItem("vis_perm",false)
     sessionStorage.setItem('state_num', 1);
@@ -122,5 +14,13 @@ function data_upload_check(){
     else{
         alert("Please upload your dataset in order to continue visualizing!")
     }
+
+}
+
+function state_num(){
+
+    var state_numbers = document.getElementById("state_number").value;
+
+    sessionStorage.setItem('state_num', state_numbers);
 
 }
