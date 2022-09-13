424f18c12411c6961a91e139eb09952f9ba3fb59
turhanberk <berkturhan@sabanciuniv.edu>
2022-09-13 22:22:44 +0300

    initial animation functions are implemented

---
+++ js/animation.js
@@ -0,0 +1,21 @@
+var animationIndex = -1
+var animationActive = false
+var animationData = {};
+
+
+function createNodeAnimation(node){
+
+    return true;
+}
+
+function linkColorAnimation(link){
+
+    return true;
+}
+
+
+function linkWidthAnimation(link){
+
+
+    return true
+}
